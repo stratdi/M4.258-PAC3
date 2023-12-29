58a30d31b756e2d99769c8f690175eeb7000688a
stratdi <cruzllado@gmail.com>
2023-12-29 18:12:15 +0100

    Header i footer replicats

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#96b15d',
+      'secondary': '#c9ff58'
+    },
+    extend: {
+      fontFamily: {
+        playpen: ["Playpen Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
-
