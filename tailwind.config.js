eacff012ea1b3580c7d3b6963eea878ed0912636
stratdi <cruzllado@gmail.com>
2023-12-31 11:51:49 +0100

    Top 6 adaptat amb Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      'primary': '#96b15d',
-      'secondary': '#c9ff58'
-    },
     extend: {
       fontFamily: {
-        playpen: ["Playpen Sans", "sans-serif"]
+        playpen: ["Playpen Sans", "sans-serif"],
+        courier: ["Courier New", "Courier", "monospace"],
+      },
+      colors: {
+        'primary': '#96b15d',
+        'secondary': '#c9ff58'
       },
       backgroundImage: {
         'gradient-radial': 'repeating-radial-gradient(circle at 0 0,#0000 0,#b9e160 40px),repeating-linear-gradient(#c9ff5855,#c9ff58);'
